aa737edf912d3fa7999da1816fb1561930b9382b
Jonathan <jhoo.alves@hotmail.com>
2023-05-09 16:01:33 -0300

    fix: corrigindo warnings nos testes de Recipes

--- src/tests/Recipes.test.js
+++ src/tests/Recipes.test.js
@@ -1,31 +1,12 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
-import mockMeals from './mock/mockMeals';
 import Meals from '../pages/Meals';
-import mockDrinks from './mock/mockDrinks';
 import Drinks from '../pages/Drinks';
 
 describe('Recipes component', () => {
   describe('Meals', () => {
-    beforeEach(() => {
-      jest.spyOn(global, 'fetch').mockResolvedValue({
-        json: async () => (mockMeals),
-      });
-    });
-
-    it('Teste se fetch é chamado', async () => {
-      act(() => {
-        render(<Meals />);
-      });
-
-      expect(fetch).toHaveBeenCalled();
-    });
-
-    it('Teste se os componentes são renderizados na pagina Meals', async () => {
-      act(() => {
-        render(<Meals />);
-      });
+    test('Teste se os componentes são renderizados na pagina Meals', async () => {
+      render(<Meals />);
 
       await waitFor(() => {
         const corba = screen.getByRole('heading', { name: /corba/i });
@@ -35,24 +16,8 @@ describe('Recipes component', () => {
   });
 
   describe('Drinks', () => {
-    beforeEach(() => {
-      jest.spyOn(global, 'fetch').mockResolvedValue({
-        json: async () => (mockDrinks),
-      });
-    });
-
-    it('Teste se fetch é chamado', async () => {
-      act(() => {
-        render(<Drinks />);
-      });
-
-      expect(fetch).toHaveBeenCalled();
-    });
-
-    it('Teste se os componentes são renderizados na pagina Drinks', async () => {
-      act(() => {
-        render(<Drinks />);
-      });
+    test('Teste se os componentes são renderizados na pagina Drinks', async () => {
+      render(<Drinks />);
 
       await waitFor(() => {
         const gg = screen.getByRole('heading', { name: /gg/i });
