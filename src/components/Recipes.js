353fa3fa3776c29a52af96345e7b5ecf8d673181
Jonathan <jhoo.alves@hotmail.com>
2023-05-09 09:32:10 -0300

    feat: botões de categoria [Requisito 20]

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -2,24 +2,55 @@ import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import fetchApiData from '../services/fetchRecipes';
 import '../styles/Recipes.css';
+import fetchCategoryData from '../services/fetchCategory';
 
 function Recipes({ value }) {
   const [recipes, setRecipes] = useState([]);
+  const [category, setCategory] = useState([]);
 
   useEffect(() => {
     const fetchRecipes = async () => {
       const data = await fetchApiData(value);
       setRecipes(data);
     };
+    const fetchCategory = async () => {
+      const data = await fetchCategoryData(value);
+      console.log(data);
+      setCategory(data);
+    };
     fetchRecipes();
+    fetchCategory();
   }, [value]);
 
-  const MAX_RESULTS = 12;
-  const results = recipes.slice(0, MAX_RESULTS);
+  const MAX_RECIPES = 12;
+  const results = recipes.slice(0, MAX_RECIPES);
+
+  const MAX_CATEGORY = 5;
+  const categoryResults = category.slice(0, MAX_CATEGORY);
 
   return (
     <div>
       Eu sou o componente recipes
+      {value === 'Meals' && (
+        categoryResults?.map((cat, index) => (
+          <button
+            data-testid={ `${cat.strCategory}-category-filter` }
+            key={ index }
+          >
+            {cat.strCategory}
+          </button>
+        ))
+      )}
+      {value === 'Drinks' && (
+        categoryResults?.map((cat, index) => (
+          <button
+            data-testid={ `${cat.strCategory}-category-filter` }
+            key={ index }
+          >
+            {cat.strCategory}
+          </button>
+        ))
+      )}
       {value === 'Meals' && (
         results?.map((recipe, index) => (
           <div
