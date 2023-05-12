64dadad7586d8674153dbeee34bc6cd576037619
Katia Nienow <nienowkatia@gmail.com>
2023-05-12 20:21:46 -0300

    recipes atualizado conforme a branch do Marcos e requisito 11 - ok

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Provider from './contexts/Provider';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
   );
 
 // If you want your app to work offline and load faster, you can change
