c72d3e4adb74d42c778c2fa81d55e88f43eba401
Marcos Vinicius <marcos.11_vinicius@hotmail.com>
2023-05-12 04:16:34 -0300

    feat: tela de detalhes das receitas [reqs do 24 ao 31, menos o 27 e o 29 que precisa dos 45% de cobertura de testes]

--- src/pages/RecipeDetails/index.js
+++ src/pages/RecipeDetails/index.js
@@ -1,58 +1,71 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
+import './Style.css';
 
 export default function RecipeDetails() {
+  const { location: { pathname }, push } = useHistory();
+  const [buttonText, setButtonText] = useState('Start Recipe');
+  const [hideButton, setHideButton] = useState(false);
+
   const [recipeData, setRecipeData] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
   const [recommendations, setRecommendations] = useState([]);
-  const { location: { pathname } } = useHistory();
   const isMeal = pathname.includes('meals');
 
   const vinte = 20;
   const menosOnze = -11;
   const seis = 6;
 
+  const getEndPoint = (path, id) => ({
+    endPoint: `https://www.${path.includes('drinks') ? 'thecocktaildb' : 'themealdb'}.com/api/json/v1/1/lookup.php?i=${id}`,
+    suggestion: `https://www.${path.includes('drinks') ? 'themealdb' : 'thecocktaildb'}.com/api/json/v1/1/search.php?s=`,
+  });
+
+  const fetchAPI = useCallback(async () => {
+    const id = pathname.split('/').pop();
+    const { endPoint, suggestion } = getEndPoint(pathname, id);
+
+    try {
+      const [dataRecipes, dataRecommendations] = await Promise.all([
+        fetch(endPoint).then((res) => res.json()),
+        fetch(suggestion).then((res) => res.json()),
+      ]);
+
+      if (pathname.includes('meals')) {
+        setRecipeData(dataRecipes.meals);
+        setRecommendations(dataRecommendations.drinks);
+      }
+
+      if (pathname.includes('drinks')) {
+        setRecipeData(dataRecipes.drinks);
+        setRecommendations(dataRecommendations.meals);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [pathname]);
+
+  useEffect(() => {
+    fetchAPI();
+  }, [fetchAPI]);
+
   useEffect(() => {
     const id = pathname.split('/').pop();
-    const apiUrl = isMeal
-      ? `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
-      : `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`;
-    const fetchData = async () => {
-      setIsLoading(true);
-      try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        setRecipeData(data);
-      } catch (error) {
-        console.error(error);
-      }
-      setIsLoading(false);
-    };
-    fetchData();
-  }, [pathname, isMeal]);
-
-  useEffect(() => {
-    const fetchRecommendations = async () => {
-      try {
-        const response = await fetch(
-          isMeal
-            ? 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
-            : 'https://www.themealdb.com/api/json/v1/1/search.php?s=',
-        );
-        const data = await response.json();
-        setRecommendations(isMeal ? data.drinks : data.meals);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchRecommendations();
-  }, [isMeal]);
-
-  if (isLoading) return <div>Loading...</div>;
+    const doneRecipesObject = localStorage.getItem('doneRecipes') || [];
+    if (doneRecipesObject.length !== 0) {
+      const doneRecipes = JSON.parse(doneRecipesObject);
+      const check = doneRecipes.some((done) => Number(done.id) === Number(id));
+      if (check) setHideButton(check);
+    }
+    const inProgressRecipes = JSON.parse(localStorage.getItem('inProgressRecipes')) || {};
+    const recipeInProgress = inProgressRecipes[isMeal ? 'meals' : 'drinks'];
+    if (recipeInProgress) {
+      setButtonText('Continue Recipe');
+    }
+  }, [isMeal, pathname]);
 
   if (!recipeData) return null;
 
-  const recipe = isMeal ? recipeData.meals[0] : recipeData.drinks[0];
+  const recipe = recipeData[0];
   const ingredients = [];
   for (let i = 1; i <= vinte; i += 1) {
     if (recipe[`strIngredient${i}`]) {
@@ -63,18 +76,31 @@ export default function RecipeDetails() {
     }
   }
 
+  const handleClick = () => {
+    const route = pathname.includes('meals')
+      ? `/meals/${recipe.idMeal}/in-progress` : `/drinks/${recipe.idDrink}/in-progress`;
+    push(route);
+  };
+
   return (
-    <div>
+    <div className="recipe-details">
       <img
         src={ recipe.strMealThumb || recipe.strDrinkThumb }
         alt="Recipe"
         data-testid="recipe-photo"
+        className="recipe-photo"
       />
-      <h2 data-testid="recipe-title">{recipe.strMeal || recipe.strDrink}</h2>
-      <p data-testid="recipe-category">
+      <h2
+        data-testid="recipe-title"
+        className="recipe-title"
+      >
+        {recipe.strMeal || recipe.strDrink}
+
+      </h2>
+      <p data-testid="recipe-category" className="recipe-category">
         {isMeal ? recipe.strCategory : recipe.strAlcoholic}
       </p>
-      <ul>
+      <ul className="ingredients-list">
         {ingredients.map((ingredient, index) => (
           <li
             key={ ingredient.name }
@@ -88,22 +114,58 @@ export default function RecipeDetails() {
           </li>
         ))}
       </ul>
-      <p data-testid="instructions">{recipe.strInstructions}</p>
+      <p data-testid="instructions" className="instructions">{recipe.strInstructions}</p>
       {isMeal && (
         <iframe
           src={ `https://www.youtube.com/embed/${recipe.strYoutube.slice(menosOnze)}` }
           title="Recipe video"
           data-testid="video"
+          className="video"
         />
       )}
-      <h3>Recommendations</h3>
-      <ul>
-        {recommendations.slice(0, seis).map((recommendation) => (
-          <li key={ recommendation.idMeal || recommendation.idDrink }>
-            {recommendation.strMeal || recommendation.strDrink}
-          </li>
-        ))}
-      </ul>
+      <section className="carousel">
+        {
+          recommendations?.slice(0, seis).map((suggestion, index) => (
+            <div
+              className="carousel-card"
+              data-testid={ `${index}-recommendation-card` }
+              key={ index }
+            >
+              <img
+                src={
+                  pathname?.includes('meals')
+                    ? suggestion.strDrinkThumb
+                    : suggestion.strMealThumb
+                }
+                alt={
+                  pathname?.includes('meals')
+                    ? suggestion.strDrink
+                    : suggestion.strMeal
+                }
+              />
+              <p data-testid={ `${index}-recommendation-title` }>
+                {
+                  pathname?.includes('meals')
+                    ? suggestion.strDrink
+                    : suggestion.strMeal
+                }
+              </p>
+            </div>))
+        }
+      </section>
+      { !hideButton
+      && (
+        <button
+          type="button"
+          data-testid="start-recipe-btn"
+          className="start-recipe-btn"
+          onClick={ handleClick }
+          style={ { position: 'fixed', bottom: 0 } }
+        >
+          {buttonText}
+        </button>
+      )}
+
     </div>
   );
 }
